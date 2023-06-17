701480f37102e8f6403b1f283d06e30839a439bc
Mike Njuki <mikenjuki@gmail.com>
2023-06-17 14:47:24 +0300

    Clean up v6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,15 @@ import BodyColorUpdater from "./components/BodyColorUpdater";
 
 const Josefin = Josefin_Sans({ weight: ["400", "700"], subsets: ["latin"] });
 
-export const metadata = {
-  title: "ToDo App",
-  description: "ToDo App",
-  authors: { name: "Mike Njuki", url: "https://blog.mikenjuki.com" },
-  og: {
-    title: "ToDo App",
-    type: "website",
-  },
-};
+// export const metadata = {
+//   title: "ToDo App",
+//   description: "ToDo App",
+//   authors: { name: "Mike Njuki", url: "https://blog.mikenjuki.com" },
+//   og: {
+//     title: "ToDo App",
+//     type: "website",
+//   },
+// };
 
 export default function RootLayout({
   children,
